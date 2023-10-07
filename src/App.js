b9990fc8f48667d22198e83730f1aaadecac73cb
Mohammed Sinan K <mohammedsinan.k.33@gmail.com>
2023-10-08 01:24:14 +0530

    Login

--- src/App.js
+++ src/App.js
@@ -3,23 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <div className="nav_bar">
+          <div className="right">
+            <h1>Waste Up</h1>
+          </div>
+        </div>
+      </nav>
+      <main>
+        <div className="main">
+          <div className="left">
+            <h2>Sign in to Waste Up</h2>
+            <button className="button1">
+              <img src="icons8-google-20.png" alt="" className="icon" /> Sign in with Google
+            </button>
+            <div className="centre">
+              <hr className="devider" />
+              <p>or</p>
+              <hr className="devider2" />
+            </div>
+            <button className="button2"> Continue with email</button>
+          </div>
+          <div className="right2">
+            <img src="7.jpg" alt="" />
+          </div>
+        </div>
+      </main>
     </div>
-  );
+);
 }
 
 export default App;
